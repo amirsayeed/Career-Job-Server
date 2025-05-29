46aa57c5d8c3bef6b0ef0bbca952056b0ac84268
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-05-29 13:22:04 +0600

    created get api to get applications by applicant's email

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ async function run() {
 
         //job_applications
 
+        app.get('/applications', async (req, res) => {
+            const email = req.query.email;
+            const query = {
+                applicant: email
+            };
+            const result = await applicationsCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post('/applications', async (req, res) => {
             const application = req.body;
             console.log(application);
