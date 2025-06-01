b0334698cf3dc7329763ff147c5602edbf166934
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-06-01 11:44:57 +0600

    connect two collection without aggregate and show application_count for added job by hr

--- index.js
+++ index.js
@@ -45,6 +45,23 @@ async function run() {
         })
 
 
+        app.get('/jobs/applications', async (req, res) => {
+            const email = req.query.email;
+            const query = {
+                hr_email: email
+            };
+            const jobs = await jobsCollection.find(query).toArray();
+
+            for (const job of jobs) {
+                const applicationQuery = {
+                    jobId: job._id.toString()
+                };
+                const application_count = await applicationsCollection.countDocuments(applicationQuery);
+                job.application_count = application_count;
+            }
+            res.send(jobs);
+        })
+
         app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
@@ -59,6 +76,7 @@ async function run() {
             const result = await jobsCollection.insertOne(newJob);
             res.send(result);
         })
+
 
         //job_applications
 
