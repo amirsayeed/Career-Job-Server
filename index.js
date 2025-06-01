52d7abf8a54f63805bf28877714bb37f6365e38a
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-06-01 13:19:09 +0600

    jwt token created and send to client side

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const {
     ServerApiVersion,
     ObjectId
 } = require('mongodb');
+const jwt = require('jsonwebtoken')
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -32,6 +33,23 @@ async function run() {
         const jobsCollection = client.db("careerJob").collection("jobs");
         const applicationsCollection = client.db("careerJob").collection("applications");
 
+        //jwt token related api
+        app.post('/jwt', async (req, res) => {
+            const {
+                email
+            } = req.body;
+            const user = {
+                email
+            };
+            const token = jwt.sign(user, 'secret', {
+                expiresIn: '1h'
+            });
+            res.send({
+                token
+            });
+        })
+
+        //jobs api
         app.get('/jobs', async (req, res) => {
             const email = req.query.email;
             const query = {};
